1c1f6fc9eff8d05936d5f0e8d9b84de786a3e56f
virtual mr. so <soteched2017@gmail.com>
2020-09-29 00:24:25 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+/**
+ * examples for computer science, 12 
+ * Toronto, Canada
+ *
+ * modified     20200928
+ * date         20200928
+ * @filename	Main.java
+ * @author      vmso - virtual mr. so
+ * @version     1.0
+ * @see         session 11 - package slide deck
+ */
+
+// notice: no package
+
+public class Main {
+
+    public static void main (String[] args) {
+        System.out.println("Main: hi");        
+    }
+    
+}
---
+++ src/MainSrc.java
@@ -0,0 +1,21 @@
+/**
+ * examples for computer science, 12 
+ * Toronto, Canada
+ *
+ * modified     20200928
+ * date         20200928
+ * @filename	MainSrc.java
+ * @author      vmso - virtual mr. so
+ * @version     1.0
+ * @see         session 11 - package slide deck
+ */
+
+package com.peelsb.student;
+
+public class MainSrc {
+
+    public static void main (String[] args) {
+        System.out.println("com.peelsb.student.MainSrc: hi");        
+    }
+    
+}
